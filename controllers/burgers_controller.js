f821a61662eb75ee38a0d2ea98e2a32dc43fb375
Elvis Pham <epha1206@gmail.com>
2018-05-23 13:53:34 -0400

    added controller

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,39 @@
+//dependencies
+var express = require("express");
+var router = express.Router();
+
+//Import model
+var burger = require("../models/burger.js");
+
+//create routes
+router.get('/', function(req, res) {
+    burger.selectAll(function(data) {
+        var hbsObject = {
+            burgers: data
+        };
+        res.render('index', hbsObject);
+    });
+});
+
+router.post('/burgers', function(req, res) {
+    burger.insertOne([
+        'burger_name'
+    ], [
+        req.body.burger_name
+    ], function(data) {
+        res.redirect('/');
+    });
+});
+
+router.put('/burgers/:id', function(req, res) {
+    var condition = 'id= ' + req.params.id;
+
+    burger.updateOne({
+        devoured: true
+    }, condition, function(data) {
+        res.redirect('/');
+    });
+});
+
+//export routes
+module.exports = router;
